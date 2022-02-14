332ef27a5c63c6c8a7d2aab3587d96c11f362a42
Miaomiao Liu <mia@blankt.com>
2022-02-14 10:22:23 +0100

    Combination

---
+++ FestivalGames/Assets/Scripts/Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Weapon : MonoBehaviour
+{
+   
+    public float SwordVelocity;
+    Vector3 lastdirectionRef;
+    public Vector3 direct;
+    public XRController xRController;
+    public bool canSlice = false;
+    public GameObject slicePanel;
+    private void Start()
+    {
+        xRController = transform.GetComponentInParent<XRController>();
+    }
+    public void Vibrate(float time)
+    {
+        xRController.SendHapticImpulse(1, time);
+    }
+    // manually calculate  velocity
+    public void Update()
+    {
+        direct = transform.position - lastdirectionRef;
+        lastdirectionRef = transform.position;
+        SwordVelocity = direct.magnitude / Time.deltaTime;
+
+        direct = direct.normalized;
+        //when velocity reach 60, the sword can slice items
+        if (Vector3.Angle(direct, transform.forward) <= 60)
+        {
+            canSlice = true;
+        }
+        else
+        {
+            canSlice = false;
+        }
+    }
+}
+
