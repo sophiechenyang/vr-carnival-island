323da118467cd157e2bb9910fd22418de4a76efd
Sophie Chen Yang <yangchensophie@gmail.com>
2022-02-15 09:04:41 +0100

    Move deer to carrot
    A carrot is spawned when the user toss in a coin, the deer then moves to the carrot

--- Festival Island/Assets/Scripts/AttractAnimal.cs
+++ Festival Island/Assets/Scripts/AttractAnimal.cs
@@ -3,27 +3,27 @@ using UnityEngine;
 public class AttractAnimal : MonoBehaviour
 {
     private float speed = 1.0f;
-    public GameObject sphere;
+    private float step; 
     public GameObject deer;
     public GameObject moose;
+    public GameObject carrot;
+    public Vector3 offset;
+    public Vector3 offset2;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("food"))
         {
-            sphere.SetActive(true);
-            float step = speed * Time.deltaTime;
-            deer.transform.position = Vector3.MoveTowards(deer.transform.position, other.transform.position, step);
-            moose.transform.position = Vector3.MoveTowards(moose.transform.position, other.gameObject.transform.position, step);
         }
     }
     void Start()
     {
-        
+        step = speed * Time.deltaTime;
     }
 
     void Update()
     {
-        
+        deer.transform.position = Vector3.MoveTowards(deer.transform.position, carrot.transform.position + offset, step);
+        moose.transform.position = Vector3.MoveTowards(moose.transform.position, carrot.transform.position + offset2, step);
     }
 }
